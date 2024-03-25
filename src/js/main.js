acbb248892ea21659d4c2fd64f0e0160397c3169
Vadym <vaddimmura@gmail.com>
2024-03-25 13:41:02 +0200

    Before restart PC

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,16 @@
 jQuery(function () {
-    // Додавання обробників подій для кнопок '.search-btn' та '.close'
-    $(".search-btn, .close").on("click", function (event) {
+    $(".burger-btn, .sidebar__close, .overlay").on("click", function (event) {
+        event.preventDefault(); // Запобігаємо перезавантаженню сторінки при натисканні на кнопку
+        $(".sidebar").toggleClass("active"); // Перемикаємо клас 'active' для '.search'
+        $("body").toggleClass("no-scroll"); // Перемикаємо клас 'active' для '.search'
+        $("header, main").toggleClass("padding"); // Перемикаємо клас 'active' для '.search'
+        $(".search").removeClass("active"); // Перемикаємо клас 'active' для '.search'
+    }); // Додавання обробників подій для кнопок '.search-btn' та '.close'
+
+    $(".search-btn, .search__close").on("click", function (event) {
         event.preventDefault(); // Запобігаємо перезавантаженню сторінки при натисканні на кнопку
         $(".search").toggleClass("active"); // Перемикаємо клас 'active' для '.search'
+        $(".sidebar").removeClass("active"); // Перемикаємо клас 'active' для '.search'
 
         // Викликаємо функцію з фокусом після завершення анімації
         $(".search").one("transitionend", function () {
@@ -11,9 +19,4 @@ jQuery(function () {
             }
         });
     });
-
-  
 });
-
-
-
