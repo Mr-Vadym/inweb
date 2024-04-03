7cac698daa63051c3adec02026624a20dde5879b
Vadym <vaddimmura@gmail.com>
2024-04-03 08:04:21 +0300

    Fix adaptive slider

--- src/js/main.js
+++ src/js/main.js
@@ -28,13 +28,13 @@ jQuery(function () {
         autoplaySpeed: 2000,
         arrows: true,
         slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToScroll: 1,
         responsive: [
             {
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
                     infinite: true,
                     dots: true,
                     arrows: false,
@@ -43,8 +43,8 @@ jQuery(function () {
             {
                 breakpoint: 600,
                 settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
+                    slidesToShow: 1,
+                    slidesToScroll: 1
                 }
             },
             {
