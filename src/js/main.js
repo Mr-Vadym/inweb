6469d455ea60609d2e141e6a496c1b0a39afa1fa
Vadym <vaddimmura@gmail.com>
2024-04-03 04:48:54 +0300

    Fix slider

--- src/js/main.js
+++ src/js/main.js
@@ -21,36 +21,43 @@ jQuery(function () {
         });
     });
 
-
     $(".more-slider__articles").slick({
-
-
-        slidesToShow: 3,
-        slidesToScroll: 1,
+        infinite: true,
+        speed: 700,
         autoplay: true,
         autoplaySpeed: 2000,
-
-        responsive: [{
-
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 3,
-                infinite: true
+        arrows: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true,
+                    arrows: false,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
             }
-
-        }, {
-
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 2,
-                dots: true
-            }
-
-        }, {
-
-            breakpoint: 300,
-            settings: "unslick" // destroys slick
-
-        }]
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
     });
 });
+
